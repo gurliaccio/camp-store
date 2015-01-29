8cc8263f5e90b154224f94701413ece5833bc943
Michael Gurliaccio <gurliaccio@gmail.com>
2015-01-29 16:52:54 -0500

    saywha

---
+++ model/item.rb
@@ -0,0 +1,21 @@
+# This class represents a single Item you can purchase in the store.
+class Item
+  attr_reader :id, :category, :name, :price
+  attr_reader :rating, :description, :image_file
+
+  def initialize(args)
+    @id          = args[:id]
+    @category    = args[:category]
+    @name        = args[:name]
+    @price       = args[:price]
+    @rating      = args[:rating]
+    @description = args[:description]
+    @image_file  = args[:image_file]
+  end
+
+  def to_s
+    "ID: #{@id}, Category: #{@category}, Name: #{@name}, " \
+    "Price: #{@price}, Description: #{@description}, " \
+    "Image File: #{@image_file}"
+  end
+end
